c0b6fa592cd86b27d10337596d21353a20c4174e
SDS Cluster Group 1 <238189528+socialsciencedashboard-hub@users.noreply.github.com>
2025-10-29 20:43:43 +0100

    Print cleanout

--- youtube/src/download_youtube.py
+++ youtube/src/download_youtube.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     try:
         download_from_playlist(playlist_url, bundestag=bundestag)
     except KeyboardInterrupt:
-        print("\n⏹️  Download stopped by user.")
+        print("Download stopped manually")
