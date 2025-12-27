85d49ea28269440b78707589368783f3ac5d7a2a
SDS Cluster Group 1 <238189528+socialsciencedashboard-hub@users.noreply.github.com>
2025-12-27 12:10:18 +0100

    added PO token WEB for downloading youtubes

--- youtube/src/yt_utils.py
+++ youtube/src/yt_utils.py
@@ -170,7 +170,7 @@ def download_from_playlist(playlist_url, bundestag: bool = True, talkshow_name: 
             try:
                 # download audio
                 # yt = YouTube(url, client = "ANDROID_EMBED", on_progress_callback=on_progress)
-                yt = YouTube(url, on_progress_callback=on_progress)
+                yt = YouTube(url, "WEB", on_progress_callback=on_progress)
 
                 # check whether the video is not a short (short is less than 4 minutes)
                 if yt.length < 240:
