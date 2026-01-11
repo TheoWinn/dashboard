a7e5dffd7f96509afc417d58009850d58d4a80be
lewolfra <lewolfra@uni-bremen.de>
2026-01-11 13:26:55 +0100

    added url to database, add youtube argument to fill function

--- database/check_duplicates.py
+++ database/check_duplicates.py
@@ -1,50 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv("../topicmodelling/data/raw_topics/topics_representations_2025.csv", low_memory=False)
+df = pd.read_csv("../topicmodelling/data/raw_topics/topics_representations_2025_youtube.csv", low_memory=False)
 
-df['speech_start'] = df[['transcript_start', 'start']].max(axis=1)
-df['speech_end'] = df[['transcript_end', 'end']].max(axis=1)
+# df['speech_start'] = df[['transcript_start', 'start']].max(axis=1)
+# df['speech_end'] = df[['transcript_end', 'end']].max(axis=1)
 # df['speech_duration'] = round((df['speech_end'] - df['speech_start']), 5)
-df['speech_duration'] = (df['speech_end'] - df['speech_start'])
+df['speech_duration'] = (df['end'] - df['start'])
 
 print(df.shape)
 
-sub = df[["filename", 
-          "protokoll_name", 
-          "topic", 
-          "speech_duration",
-          "text",
-          "protokoll_text",
-          "transcript_text",
-          "source",
-          "transcript_speaker",
-          "similarity", 
-          "speech_start",
-          "speech_end",
-          "protokoll_party",
-          "transcript_start",
-          "transcript_end",
-          "date",
-          "cluster_idx",
-          "speaker",
-          "start",
-          "end",
-          "words",
-          "speaker_block",
-          "probability",
-          "Representation",
-          "protokoll_docid",]]
-
-print(sub.shape)
-
-dups = sub[sub.duplicated(
-    subset=["text", "filename", "protokoll_name", "topic", "speech_duration"],
+dups = df[df.duplicated(
+    subset=["text", "filename", "Topic", "speech_duration"],
     keep=False
 )]
 
-unique_speeches = sub[["text", "filename", "protokoll_name", "topic", "speech_duration"]].drop_duplicates().shape[0]
+unique_speeches = df[["text", "filename", "Topic", "speech_duration"]].drop_duplicates().shape[0]
 print("Number of unique speech_text entries:", unique_speeches)
 
-dups = dups.sort_values(["text", "filename", "protokoll_name", "topic", "speech_duration"])
+dups = dups.sort_values(["text", "filename", "Topic", "speech_duration"])
 
 dups.to_excel("../data/check_duplicates.xlsx", index=False)
--- database/insert.py
+++ database/insert.py
@@ -1,4 +1,4 @@
-from db_utils import fill_db, views_db, comment_db
+from db_utils import fill_db, views_db, comment_db, rebuild_db
 from dotenv import load_dotenv
 import os
 import psycopg2
@@ -18,8 +18,11 @@ DB_URL = os.environ["DATABASE_URL"]
 
 # views_db(DB_URL)
 
-comment_db(DB_URL)
+# comment_db(DB_URL)
 
 # supabase = create_client(os.environ["SUPABASE_URL"], os.environ["SUPABASE_ANON_KEY"])
 # resp = supabase.schema("dashboard").rpc("topics_metrics_all_xweek_windows", {"p_year": 2025, "p_window_weeks": 4}).select("topic_duration_ts").eq("topic_id", "1").execute()
 # print(resp.data)
+
+##### restart database from scratch
+rebuild_db(DB_URL, "../topicmodelling/data/raw_topics/topics_representations_2025_youtube.csv", True)
