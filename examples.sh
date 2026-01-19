7b2de8d8e0a4851b6b47815a9b1485f0e7e67634
lewolfra <lewolfra@uni-bremen.de>
2026-01-19 19:22:07 +0100

    worked on more examples

--- examples.sh
+++ examples.sh
@@ -12,10 +12,42 @@ fi
 
 # get 10 most salient topics in Bundestag
 curl -sS -G "https://${PROJECT_ID}.supabase.co/rest/v1/topics_view" \
-  -d "select=topic_label,topic_duration_bt,bt_normalized_perc,mismatch_ppoints,mismatch_log_ratio" \
+  -d "select=topic_label,bt_normalized_perc,ts_normalized_perc,mismatch_ppoints,mismatch_log_ratio" \
   -d "order=bt_normalized_perc.desc" \
   -d "limit=10" \
   -H "apikey: ${ANON_KEY}" \
   -H "Authorization: Bearer ${ANON_KEY}" \
   -H "Accept-Profile: dashboard" \
 | python3 -m json.tool
+
+# get 10 most salient topics in Talkshows
+curl -sS -G "https://${PROJECT_ID}.supabase.co/rest/v1/topics_view" \
+  -d "select=topic_label,ts_normalized_perc,bt_normalized_perc,mismatch_ppoints,mismatch_log_ratio" \
+  -d "order=ts_normalized_perc.desc" \
+  -d "limit=10" \
+  -H "apikey: ${ANON_KEY}" \
+  -H "Authorization: Bearer ${ANON_KEY}" \
+  -H "Accept-Profile: dashboard" \
+| python3 -m json.tool
+
+# get top 5 topics with higher salience in bundestag than talkshows
+curl -sS -G "https://${PROJECT_ID}.supabase.co/rest/v1/topics_view" \
+  -d "select=topic_label,bt_normalized_perc,ts_normalized_perc,mismatch_ppoints,mismatch_log_ratio" \
+  -d "order=mismatch_ppoints.desc" \
+  -d "limit=5" \
+  -H "apikey: ${ANON_KEY}" \
+  -H "Authorization: Bearer ${ANON_KEY}" \
+  -H "Accept-Profile: dashboard" \
+| python3 -m json.tool
+
+# get top 5 topics with higher salience in bundestag than talkshows
+curl -sS -G "https://${PROJECT_ID}.supabase.co/rest/v1/topics_view" \
+  -d "select=topic_label,ts_normalized_perc,bt_normalized_perc,mismatch_ppoints,mismatch_log_ratio" \
+  -d "order=mismatch_ppoints.asc" \
+  -d "limit=5" \
+  -H "apikey: ${ANON_KEY}" \
+  -H "Authorization: Bearer ${ANON_KEY}" \
+  -H "Accept-Profile: dashboard" \
+| python3 -m json.tool
+
+
